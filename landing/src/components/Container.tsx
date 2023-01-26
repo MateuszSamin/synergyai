cb8dc3b2cfd2a2f8e90822abed01a37c4f839b40
karolproksa <karolproksa@gmail.com>
2023-01-26 14:07:25 +0100

    Reusable components + colors

---
+++ landing/src/components/Container.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = (props: any) => {
+  return (
+    <div className={`max-w-[1500px] mx-auto px-10 w-full ${props.className}`}>{props.children}</div>
+  )
+}
+
+export default Container
--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#001122',
+        'green': '#01C38E',
+        'purple': '#5564DB'
+      }
+    },
   },
   plugins: [],
 }
