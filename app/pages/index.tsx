a858950324da51a61c46075dcfb791689be208dc
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2023-03-29 19:00:15 +0200

    add sidebar

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import MainScreen from '../components/MainScreen'
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <MainScreen />
+      <Sidebar />
     </div>
   )
 }
